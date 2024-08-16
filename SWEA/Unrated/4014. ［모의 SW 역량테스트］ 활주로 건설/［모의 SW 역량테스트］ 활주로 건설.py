dc56b83c23e921a2f1774a407c9432d343d7cc41
ChanHo Jeong <74286106+chano275@users.noreply.github.com>
2024-08-16 13:18:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 171 ms, Memory: 49,796 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.py
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.py
@@ -1,53 +1,50 @@
+# import sys
+# sys.stdin = open('sample_input (5).txt')
+
+
+def flight(arr):
+    cnt = 1
+    for i in range(1, len(arr)):
+        if arr[i-1] == arr[i]:
+            cnt += 1
+            if cnt > x:  cnt = x
+
+        elif arr[i-1] == arr[i] + 1:  # 하나 감소
+            if cnt < 0:  return 0
+            cnt = -x + 1  # 체크한 곳부터 x번동안 동일한 원소가 왜야 하므로, -x 에 + 1 해서 카운트에 줌
+
+        elif arr[i-1] == arr[i] - 1:  # 하나 증가
+            if cnt == x:
+                cnt = 1
+            else:
+                return 0
+        else: return 0
+
+    if cnt >= 0:
+        return 1
+    return 0
+
 T = int(input())
 for tc in range(1, T+1):
     n, x = map(int, input().split())
-    flight = [list(map(int, input().split())) for _ in range(n)]
-
-    def checker(line):
-        cnt, chk = 1, -1
-
-        if len(line) < x : return 0
-
-        for i in range(1, len(line)):
-
-            if line[i] == line[i - 1]:
-                cnt += 1
-
-            elif line[i] == line[i - 1] + 1:
-                if cnt >= x:
-                      cnt = 1
-                else: 
-                    return 0
-
-            elif line[i] == line[i - 1] - 1:
-                if cnt < 0: 
-                    return 0
-                else:
-                    cnt = -x + 1
-
-            else: 
-                return 0 # 차이 2 +- 이상 > 틀림
-
-        if cnt >= 0:
-            return 1
-        else:
-            return 0
-
-
+    maps = [list(map(int, input().split())) for _ in range(n)]
 
     ans = 0
 
-    for i in range(n): 
-        ans += checker(flight[i])
-        ans += checker([q[i] for q in flight])
-        # print('###############################')
-        # print(flight[i])
-        # print(checker(flight[i]))
-        # print('###############################')
-        # print([q[i] for q in flight])
-        # print(checker([q[i] for q in flight]))
-        # print('###############################')
-        
-    print(f'#{tc} {ans}')
+    for i in range(n):
+        temp = []
+        for j in range(n):
+            temp.append(maps[i][j])
+        ans += flight(temp)
 
-    #break
+
+    # print('########################')
+
+    for i in range(n):
+        temp = []
+        for j in range(n):
+            temp.append(maps[j][i])
+        ans += flight(temp)
+
+
+    print(f'#{tc} {ans}')
