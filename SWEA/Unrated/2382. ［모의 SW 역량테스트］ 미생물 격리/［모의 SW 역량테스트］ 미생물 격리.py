c56c178c0ff0cf9b58c567e5f34c91382674f8a6
ChanHo Jeong <74286106+chano275@users.noreply.github.com>
2024-08-18 09:20:50 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 618 ms, Memory: 61,140 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.py
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.py
@@ -1,60 +1,63 @@
+
+
+dxy = [(1,0), (0,1), (-1,0), (0,-1)]
 T = int(input())
 for tc in range(1, T+1):
-    n,m,k = list(map(int, input().split()))
-    micro_input = [list(map(int , input().split())) for _ in range(k)]
+    n, m, k = list(map(int, input().split()))
+    micros_list = [list(map(int, input().split())) for _ in range(k)]
 
-    back = {}
-    forward = {}
+    first_dict = {}
+    # (x, y) = [ (수, 방향) , ...]
+    for temp in micros_list:
+        first_dict[(temp[0], temp[1])] = [(temp[2], temp[3])]
 
-    # 1 2 3 4 상 하 좌 우
-    dxy = [(-1,0), (1,0), (0,-1), (0,1)]
-
-    for micro in micro_input:
-        back[(micro[0], micro[1])] = [(micro[2], micro[3])]
-
-    for _ in range(m): # m 시간 동안 반복 > 끝나고 살아남은 미생물 총합 구하기
-        forward = {}
-
-        # x, y dir 확인하고 이동시켜야지
-        for k, v in back.items():
+    for _ in range(m):
+        second_dict = {}
+        for k, v in first_dict.items():
             cx, cy = k
-            num, dire = v[0]
-            if dire == 1:cx -= 1 # 상 > cy num dire 그대로
-            elif dire == 2: cx += 1
-            elif dire == 3: cy -= 1
-            else: cy += 1
+            if v[0][1] == 1: dx, dy = dxy[2]
+            elif v[0][1] == 2: dx, dy = dxy[0]
+            elif v[0][1] == 3: dx, dy = dxy[3]
+            elif v[0][1] == 4: dx, dy = dxy[1]
+            nx, ny = cx + dx, cy + dy
 
-            if cx == 0 or cx == n-1 or cy == 0 or cy == n-1:
-                num //= 2
-                if dire == 1: dire = 2
-                elif dire == 2: dire = 1
-                elif dire == 3: dire = 4
-                elif dire == 4: dire = 3
-    
-            if num > 0:
-                if (cx, cy) not in forward: #
-                    forward[(cx, cy)] = [(num, dire)]
-                else:
-                    forward[(cx, cy)].append((num, dire))
-
-        back = {}
-
-        for k, v in forward.items():
-            if len(v) >= 2:
-                sum_num, max_num, max_dire = 0, 0, 0
-                for (num_v, dire_v) in v:
-                    if num_v > max_num:
-                        max_num = num_v
-                        max_dire = dire_v
-                    sum_num += num_v
-                back[k] = [(sum_num, max_dire)]
+            # if nx < 0 or nx >= n or ny < 0 or ny >= n: continue
+            if nx == 0 or nx == n-1 or ny == 0 or ny == n-1:  # 벽 부딫힘
+                # 겹치는거 생각 안해도 괜찮음
+                if v[0][1] == 1:second_dict[(nx, ny)] = [(v[0][0] // 2, 2)]
+                elif v[0][1] == 2:second_dict[(nx, ny)] = [(v[0][0] // 2, 1)]
+                elif v[0][1] == 3:second_dict[(nx, ny)] = [(v[0][0] // 2, 4)]
+                elif v[0][1] == 4:second_dict[(nx, ny)] = [(v[0][0] // 2, 3)]
             else:
-                back[k] = v
+                # 벽 안부딫힘 :
+                if (nx, ny) in second_dict:
+                    second_dict[(nx, ny)].append((v[0][0], v[0][1]))
+                else:
+                    second_dict[(nx, ny)] = v
+
+        first_dict = {}
+
+
+        for k, v in second_dict.items():
+            if len(v) > 1:
+                _max, _sum, _dir = 0, 0, 0
+                for chk in v:
+                    _sum += chk[0]
+
+                    temp = _max
+                    _max = max(_max, chk[0])
+                    if temp != _max:
+                        _dir = chk[1]
+
+                first_dict[k] = [(_sum, _dir)]
+
+            else:
+                first_dict[k] = v
 
     ans = 0
-    for ans_z in back:
-        ans += back[ans_z][0][0]
+    for k, v in first_dict.items():
+        ans += v[0][0]
 
     print(f'#{tc} {ans}')
 
-
+    # break
