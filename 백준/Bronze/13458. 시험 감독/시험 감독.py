14e8f3665aa8cee09609d4b1aecaf414fdc626c9
ChanHo Jeong <74286106+chano275@users.noreply.github.com>
2024-08-30 13:45:38 +0900

    [Bronze II] Title: 시험 감독, Time: 324 ms, Memory: 232308 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -0,0 +1,19 @@
+n = int(input())
+people = list(map(int, input().split()))
+b, c = map(int, input().split())
+
+
+ans = 0
+for i in range(n):
+    ans += 1
+    target = people[i] - b
+
+    if target < 0:continue
+
+    if target % c == 0:
+        ans += (target // c)
+    else:
+        ans += ((target // c) + 1)
+
+
+print(ans)
