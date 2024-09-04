601aad708dc8596e1fc3094499781171827a58c0
ChanHo Jeong <74286106+chano275@users.noreply.github.com>
2024-09-04 10:51:56 +0900

    [Unrated] Title: [모의 SW 역량테스트] 원자 소멸 시뮬레이션, Time: 8,965 ms, Memory: 83,536 KB -BaekjoonHub

--- SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.py
+++ SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.py
@@ -1,40 +1,45 @@
-# 0 상 / 1 하 / 2 좌 / 3 우
-dxy = [(0,0.5), (0,-0.5), (-0.5,0), (0.5,0)]
+dir_dict = {  # 이동 방향은 상(0), 하(1), 좌(2), 우(3)  /  x y 순이므로 원래와 반대
+    0: (0, 0.5),
+    1: (0, -0.5),
+    2: (-0.5, 0),
+    3: (0.5, 0),
+}
+
 T = int(input())
 for tc in range(1, T+1):
-    n = int(input())
-    atoms_list = [list(map(int, input().split())) for _ in range(n)]
-    # x y dir k
-
-    ans = 0
+    n = int(input()) # 원자들의 수
+    li = [list(map(int, input().split())) for _ in range(n)]
+    # 원자 갯수   |   x y dir 에너지(k)
 
     first_dict = {}
-    for single in atoms_list:
-        first_dict[(single[0], single[1])] = [(single[2], single[3])]
+    for _l in li:
+        key = (_l[0], _l[1])
+        if key in first_dict:
+            first_dict[key].append((dir_dict[_l[2]], _l[3]))  # dir & k
+        else:first_dict[key] = [(dir_dict[_l[2]], _l[3])]
 
-    while first_dict:  # 다 비면 out
+
+    # 1,0 : [ ( ( 0, 1 ) , energy )  ]
+    ans = 0
+    while first_dict:  # 안비어있으면 ㅇㅇ
+        # print(first_dict)
         temp_dict = {}
         for k, v in first_dict.items():
-            cx, cy = k
-            dx, dy = dxy[v[0][0]]
-            nx, ny = cx + dx, cy + dy
+            moved_k = (k[0] + v[0][0][0], k[1] + v[0][0][1])
 
-            if nx < -1000 or ny < -1000 or nx > 1000 or ny >= 1000: continue
-            # 범위 밖으로 안나가면 ok
-            if (nx, ny) in temp_dict:
-                temp_dict[(nx, ny)].append(v[0]) # v[0]이 이미 튜플이야
+            if moved_k[0] < -1000 or moved_k[0] > 1000 or moved_k[1] < -1000 or moved_k[1] > 1000:continue
+            if moved_k in temp_dict:  # 소멸 아녀? > 에너지 더해줘야 하니까 한번 더
+                temp_dict[moved_k].append((v[0][0], v[0][1]))
             else:
-                temp_dict[(nx, ny)] = v
+                temp_dict[moved_k] = [(v[0][0], v[0][1])]
 
         first_dict = {}
-        # temp dict에 이동시킨 거 넣었음. 만나는거 체크
         for k, v in temp_dict.items():
             if len(v) > 1:
-                for chk in v:
-                    ans += chk[1]
+                for elem in v:
+                    ans += elem[1]
             else:
                 first_dict[k] = v
 
     print(f'#{tc} {ans}')
 
-#     break
