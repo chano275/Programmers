c6180a9a91914ca2a31609f1c5927a7b258b2e38
ChanHo Jeong <74286106+chano275@users.noreply.github.com>
2024-11-03 15:08:15 +0900

    [Gold V] Title: 경쟁적 전염, Time: 192 ms, Memory: 121116 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,24 +1,31 @@
 from collections import deque
 
+
 n, k = map(int, input().split())
 maps = [list(map(int, input().split())) for _ in range(n)]
-s, x, y = list(map(int, input().split()))
+s, x, y = list(map(int, input().split()))  # s초 뒤 x, y에 존재하는 -1 -1 해주기
 
 start_dict = {}
 
 visited = set()
 for i in range(n):
     for j in range(n):
-        if maps[i][j] != 0:
-            visited.add((i, j))
-            if maps[i][j] in start_dict:
+        if maps[i][j] != 0:  # 시작 point 
+
+            visited.add((i, j))  # 맨 처음에만 하면 좋을듯? 
+
+            if maps[i][j] in start_dict:  # 키 이미 있으면
                 start_dict[maps[i][j]].append((i, j))
             else:
                 start_dict[maps[i][j]] = [(i, j)]
 
 while s != 0:
+    # for m in maps:print(m)
+    # print('###############################')
     s -= 1
-    temp_dict = {}
+
+    temp_dict = {}   # visited 만들어놨음... k, v 돌면서 큐 채우고 
+
     queue = deque()
     for k in sorted(start_dict.keys()):
         v = start_dict[k]
@@ -30,23 +37,22 @@ while s != 0:
                 queue.append((k, v[l]))
                 visited.add(v[l])
 
+    
     while queue:
         val, (cx, cy) = queue.popleft()
         for dx, dy in [(1,0), (0,1), (-1,0), (0,-1)]:
             nx, ny = cx+dx, cy+dy 
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if (nx, ny) in visited:
-                continue
-            if maps[nx][ny] != 0:
-                continue  # 이미 바이러스가 있으면 넘어감
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:continue
+            if (nx, ny) in visited:continue  # 이게 아래랑 사실 동일한거 아닌가? 
+            if maps[nx][ny] != 0: continue
 
             maps[nx][ny] = val
+            
+            if maps[nx][ny] in temp_dict:  temp_dict[maps[nx][ny]].append((nx, ny))
+            else:                          temp_dict[maps[nx][ny]] = [(nx, ny)]
+
             visited.add((nx, ny))
-            if val in temp_dict:
-                temp_dict[val].append((nx, ny))
-            else:
-                temp_dict[val] = [(nx, ny)]
 
     start_dict = temp_dict
     
