611a6269eccd06ee86f84bb26bafa38d2342075c
ChanHo Jeong <74286106+chano275@users.noreply.github.com>
2024-11-25 17:16:29 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 211 ms, Memory: 51,928 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.py
@@ -2,46 +2,50 @@ from collections import deque
 
 
 def wisang():
-    visited = []
+    ans = []
     queue = deque()
-    for i in range(1, len_v + 1):
-        if zinip[i] == 0:  # 진입차수 없는 원소
+
+    ## 맨 처음
+    for i in range(1, len(zinip)):  # 1부터 v까지
+        if zinip[i] == 0:
             queue.append(i)
-            # visited.append()  # popleft에서 해줄까... 
-    
+            vertex_set.discard(i)
+
+
     while queue:
         cur = queue.popleft()
-        visited.append(cur)
-        for elem in graph[cur]:
-            zinip[elem] -= 1
+        ans.append(cur)
 
-        for i in range(1, len_v + 1):
-            if zinip[i] == 0 and i not in visited and i not in queue:  # 이게 되나 
+        for element in graph[cur]:  zinip[element] -= 1
+
+        for i in range(1, len(zinip)):  # 1부터 v까지
+            if zinip[i] == 0 and i in vertex_set:  #
                 queue.append(i)
+                vertex_set.discard(i)
 
-    return visited
+    return ans
+
 
 
 t = 10
 for tc in range(1, t+1):
-    len_v, len_e = map(int,input().split())
-    edges = list(map(int, input().split()))
+    v, e = map(int, input().split())
+    edge_list = list(map(int, input().split()))
+
+    zinip = [0] * (v + 1)               # 0 idx 안쓸것
+    vertex_set = set()
+    for i in range(1, v+1):vertex_set.add(i)
 
 
-    zinip = [0] * (len_v + 1)               # 진입차수 배열의 첫 원소 무시해야 
-    graph = [[] for _ in range(len_v + 1)]  # 그래프의 첫줄 무시해야 
+    graph = [[] for _ in range(v + 1)]  # 0행 안쓸 것
+    for i in range(e):
+        left, right = edge_list[2 * i], edge_list[2 * i + 1]
+        graph[left].append(right)
+        zinip[right] += 1
 
-    for i in range(len_e):
-        graph[edges[2 * i]].append(edges[2 * i + 1])  # 왼쪽 정점에서 오른쪽 정점으로 향하는 그래프 > 연결되는 
-        zinip[edges[2 * i + 1]] += 1
+    ret = wisang()
 
-    # for line in graph:
-    #     print(line)
-    # print(zinip)
-
-    ans = wisang()
-
-    print(f'#{tc} ', end = '')
-    for a in ans:
-        print(a, end = ' ')
+    print(f'#{tc}', end = ' ')
+    for elem in ret:
+        print(elem, end = ' ')
     print('')
